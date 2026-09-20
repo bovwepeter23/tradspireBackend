5b79e70e82028a8a9c5a3c96d2553900622e5444
bovwepeter23 <140882004+bovwepeter23@users.noreply.github.com>
2026-09-20 03:44:53 +0000

    cors update

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allows requests from local machine and Vercel frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Connect DB
